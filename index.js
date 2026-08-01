f97e2f2465ed31986cc279b8efb7bb49547f1417
Satvik <codesatvik@gmail.com>
2026-08-01 20:35:38 +0530

    integrate Prisma with PostgreSQL and add initial migration

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const app = express();
 app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
-const { Pool } = require("pg");
 const z = require("zod");
+const prisma = require("./prismaClient")
 const bcrypt = require("bcrypt");
 
-const pool = new Pool({
-  connectionString: process.env.neondbLink,
-});
+
 const SignupSchema = z.object({
   username: z.string().min(4),
   password: z.string().min(5),
@@ -20,9 +18,7 @@ const SignupSchema = z.object({
 app.post("/signup", async (req, res) => {
   const { data, success, error } = SignupSchema.safeParse(req.body);
   if (!success) {
-    res
-      .status(403)
-      .json({ message: "incorrect inputs", error: JSON.parse(error) });
+    res.status(403).json({ message: "incorrect inputs", error: JSON.parse(error) });
     return;
   }
   const username = data.username;
@@ -30,13 +26,16 @@ app.post("/signup", async (req, res) => {
   const email = data.email;
   const hashedPassword = await bcrypt.hash(password, 10);
   try {
-    const response = await pool.query(
-      `INSERT INTO todousers (username, password, email) VALUES ($1, $2, $3) RETURNING id`,
-      [username, hashedPassword, email],
-    );
+    const response = await prisma.todouser1.create({
+      data: {
+        username,
+        password: hashedPassword,
+        email
+      }
+    })
     res.json({
       message: "signup done",
-      id: response.rows[0].id,
+      id: response.id,
     });
   } catch (e) {
     res.status(403).json({ message: e.detail });
@@ -46,11 +45,9 @@ app.post("/signup", async (req, res) => {
 app.post("/signin", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  //const response = await pool.query(`SELECT * FROM users WHERE email="${email}" AND password="${password}"`);
-  const response = await pool.query(`SELECT * FROM todousers WHERE email=$1`, [
-    email,
-  ]);
-  const userExists = response.rows[0];
+
+  const userExists = await prisma.todouser1.findUnique({ where: {email}})
+  
 
   if (!userExists) {
     res.json({
@@ -58,8 +55,9 @@ app.post("/signin", async (req, res) => {
     });
   } else {
     const correctpassword = await bcrypt.compare(password, userExists.password);
-    const token = jwt.sign({ userId: userExists.id }, process.env.jwtSecret);
     if (correctpassword) {
+
+      const token = jwt.sign({ userId: userExists.id }, process.env.jwtSecret);
       res.json({
         message: "signed in",
         token: token,
---
+++ prismaClient.js
@@ -0,0 +1,7 @@
+require("dotenv/config")
+const { PrismaNeon } = require("@prisma/adapter-neon");
+const { PrismaClient } = require("./generated/prisma")
+
+const adapter = new PrismaNeon({connectionString: process.env.DATABASE_URL})
+const prisma = new PrismaClient({ adapter });
+module.exports = prisma;
